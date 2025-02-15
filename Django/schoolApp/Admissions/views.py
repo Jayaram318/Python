e6e4b4fb2121f43e77d769098614d7cf4e171688
Jayaram318 <jayaramireddy318@gmail.com>
2025-02-15 16:05:16 +0530

    multiple app url set3

--- Django/schoolApp/Admissions/views.py
+++ Django/schoolApp/Admissions/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 # function based views
 def addadmission(request):
-    return HttpResponse("<h1> This is the admission page </h1> <h2> welcome to the school </h2>")
+    return render(request, 'Admissions/addadmission.html')
 def admissionreport(request):
-    return HttpResponse("<h1> This is the report page </h1> <h2> welcome to the school </h2>")
+    return render(request, 'Admissions/admissionreport.html')
--- Django/schoolApp/finance/views.py
+++ Django/schoolApp/finance/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def fee(request):
-    return HttpResponse("<h1> welcome to finance</h1><h2>fill the application for pay</h2>")
+    return render(request, 'finance/fee.html')
 
 def dues(request):
-    return HttpResponse("<h1>show me the dues pending list </h1>")
+    return render(request, 'finance/dues.html')
 def financereport(request):
-    return HttpResponse("<h1>give me the complete finance list</h1>")
+    return render(request, 'finance/financereport.html')
